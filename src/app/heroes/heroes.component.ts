e200d953dd0ee48f39408533b5f94dec190f7972
Duong Do Nguyen <duongdonguyen@192.168.2.47>
2023-10-24 17:49:57 +0700

    [add] angular

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
 import {Component} from '@angular/core';
 import {Hero} from "../hero";
+import {HEROES} from "../mock_heros";
+import {HeroService} from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,9 +10,29 @@ import {Hero} from "../hero";
 })
 export class HeroesComponent {
   // hero = 'Windstorm';
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  // heroes = HEROES;
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService) {
+  }
+
+  ngOnInit(): void {
+    this.getHeroes()
+  }
+
+
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  // getHeroes(): void {
+  //   this.heroes = this.heroService.getHeroes();
+  // }
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
 
 }
